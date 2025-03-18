30f29724ec6e90ed8626a617bc545db54086ae90
unknown <villegasalrandolph@gmail.com>
2025-03-18 11:21:01 -0700

    Changed database connection from client to pool.

--- models/db_connection.js
+++ models/db_connection.js
@@ -1,10 +1,11 @@
 import dotenv from "dotenv";
+import { Pool } from "pg";
 
 dotenv.config();
 
-import pg from "pg";
+// import pg from "pg";
 
-export const db = new pg.Client({
+export const db = new Pool({
 
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
@@ -16,6 +17,19 @@ export const db = new pg.Client({
         rejectUnauthorized: false
     }
 });
+
+// export const db = new pg.Client({
+
+//     host: process.env.PGHOST,
+//     database: process.env.PGDATABASE,
+//     user: process.env.PGUSER,
+//     password: process.env.PGPASSWORD,
+//     port: process.env.PGPORT,
+//     ssl:{
+
+//         rejectUnauthorized: false
+//     }
+// });
 
 
 
